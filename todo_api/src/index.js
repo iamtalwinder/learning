12bc1973ace918753f8166742eaff2ff681a14cf
Talwinder Singh <singhtalwinder790@gmail.com>
2021-02-03 14:04:08 +0530

    sparkles: Connect with database

--- todo_api/src/index.js
+++ todo_api/src/index.js
@@ -1,5 +1,17 @@
 import app from "./server"
+import { MongoClient } from "mongodb"
 
-app.listen(process.env.PORT, () =>
-  console.log(`Listening on port ${process.env.PORT}`),
-)
+MongoClient.connect(process.env.TODO_DB_URI, {
+  poolSize: 50,
+  wtimeout: 2500,
+  useNewUrlParser: true,
+})
+  .catch((err) => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+  .then((client) => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`)
+    })
+  })
