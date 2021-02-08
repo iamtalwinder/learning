0edf80c8265393bf2e1ff25b6fa1dbd4af1faf84
Talwinder Singh <singhtalwinder790@gmail.com>
2021-02-08 11:04:51 +0530

    :sparkles: Add delete todo route and validation in create todo route

--- todo_api/src/api/todos.controller.js
+++ todo_api/src/api/todos.controller.js
@@ -5,10 +5,26 @@ export default class UserController {
     try {
       const date = new Date()
       const { todoTitle } = req.body
+      if (!todoTitle || typeof todoTitle !== "string") {
+        res
+          .status(400)
+          .json({ error: "Bad todoTitle format, expected string." })
+        return
+      }
       const userId = req.userClaim._id
 
       const result = await TodosDAO.addTodo(userId, todoTitle, date)
       res.status(201).send(await TodosDAO.getTodoById(result.insertedId))
+    } catch (e) {
+      res.status(500).send({ error: e })
+    }
+  }
+
+  static async deleteTodo(req, res) {
+    try {
+      const { todoId } = req.body
+      const userId = req.userClaim._id
+      res.status(200).send(await TodosDAO.deleteTodo(todoId, userId))
     } catch (e) {
       res.status(500).send({ error: e })
     }
--- todo_api/src/api/todos.routes.js
+++ todo_api/src/api/todos.routes.js
@@ -7,5 +7,6 @@ const router = new Router()
 router.use(Middleware.auth)
 
 router.route("/create-todo").post(todosCtrl.createTodo)
+router.route("/delete-todo").delete(todosCtrl.deleteTodo)
 
 export default router
