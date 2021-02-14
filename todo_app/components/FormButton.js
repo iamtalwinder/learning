6e47ea5d84507ab4f3b34a34ac2d3e9ab531c99c
Talwinder Singh <singhtalwinder790@gmail.com>
2021-02-14 13:29:50 +0530

    :package: Add FormButton component

---
+++ todo_app/components/FormButton.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+const FormButton = styled.TouchableOpacity`
+  background-color: #e02914;
+  margin-top: 15px;
+  padding: 8px;
+  border-radius: 20px;
+  color: white;
+  text-align: center;
+`;
+
+export default FormButton;
