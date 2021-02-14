65fe34771254ec625b06e1ca73b54801b4bd7c79
Talwinder Singh <singhtalwinder790@gmail.com>
2021-02-14 13:15:59 +0530

    :package: Add FormInput component

---
+++ todo_app/components/FormInput.js
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+const FormInput = styled.TextInput`
+  border-color: gray;
+  border-width: 1px;
+  border-radius: 20px;
+  padding: 8px;
+  margin-top: 15px;
+`;
+
+export default FormInput;
