da1136543c482d9b2f8c08621506db959c867a51
Talwinder Singh <singhtalwinder790@gmail.com>
2021-02-14 14:50:22 +0530

    :bug: Remove Text bug

--- todo_app/components/FormButton.js
+++ todo_app/components/FormButton.js
@@ -1,12 +1,22 @@
+import React from "react";
 import styled from "styled-components/native";
 
-const FormButton = styled.TouchableOpacity`
+const Button = styled.TouchableOpacity`
   background-color: #e02914;
   margin-top: 15px;
   padding: 8px;
   border-radius: 20px;
+`;
+
+const Text = styled.Text`
   color: white;
   text-align: center;
 `;
 
-export default FormButton;
+export default function FormButton({ children, onPress }) {
+  return (
+    <Button onPress={onPress}>
+      <Text>{children}</Text>
+    </Button>
+  );
+}
