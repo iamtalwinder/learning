473095c709f8757cf6021e76cc8fcffe8e55beac
Talwinder Singh <singhtalwinder790@gmail.com>
2021-02-17 12:42:55 +0530

    :sparkles: Add api login

--- todo_app/src/pages/Login.js
+++ todo_app/src/pages/Login.js
@@ -1,14 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { Alert } from "react-native";
 import { FormInput, FormButton, Container, Heading, Link } from "../components";
 import { useHistory } from "react-router-native";
+import axios from "axios";
+import {
+  Context as CurrentUserContext,
+  actionTypes as CurrentUserActionTypes,
+} from "../context/CurrentUser";
+import { API_URL } from "../constants";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const history = useHistory();
+  const CurrentUserDispatch = useContext(CurrentUserContext)[1];
 
-  const login = () => {
-    history.push("/home");
+  const login = async () => {
+    try {
+      const { data } = await axios.post(`${API_URL}/v1/user/login`, {
+        email,
+        password,
+      });
+      console.log(data);
+
+      CurrentUserDispatch({
+        type: CurrentUserActionTypes.SET_USER,
+        user: data,
+      });
+
+      history.push("/home");
+    } catch (e) {
+      console.log(e);
+      const msg = e.response
+        ? e.response.data.error
+        : "Something went wrong. Try again!";
+
+      Alert.alert("Error", msg);
+    }
   };
 
   return (
