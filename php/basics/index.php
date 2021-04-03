853e67423957da19199b6349697c0ad5cda2556a
Talwinder Singh <singhtalwinder790@gmail.com>
2021-04-03 06:03:15 +0530

    Add links in index.php

--- php/basics/index.php
+++ php/basics/index.php
@@ -5,14 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Hello World!</title>
+    <title>PHP</title>
 </head>
 
 <body>
 
-    <?php
-    echo "Hello World!";
-    ?>
+    <a href="array.php">Array</a><br />
+    <a href="form_handling.php">Form handling</a><br />
+    <a href="form_validation.php">Form validation</a><br />
+
 
 </body>
 
