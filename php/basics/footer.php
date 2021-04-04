7addeb38589bdd6f9d0a7bfda4bf2568ddbaa7a0
Talwinder Singh <singhtalwinder790@gmail.com>
2021-04-04 07:17:14 +0530

    php include

---
+++ php/basics/footer.php
@@ -0,0 +1,2 @@
+<?php
+echo "<p>Copyright &copy; 1999-" . date("Y") . " php.com</p>";
--- php/basics/include.php
+++ php/basics/include.php
@@ -5,16 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>include</title>
 </head>
 
 <body>
-
-    <a href="array.php">Array</a><br />
-    <a href="form_handling.php">Form handling</a><br />
-    <a href="form_validation.php">Form validation</a><br />
-    <a href="include.php">Include</a><br />
-
+    <?php include 'footer.php'; ?>
 </body>
 
 </html>
